c2b9ba5bf88af6cc8f311da3e542ceb1c2c44c0f
MichalR <mrudny@gmail.com>
2019-11-04 15:55:05 +0100

    deploy test

--- src/main.js
+++ src/main.js
@@ -74,6 +74,9 @@ function main() {
           <p>Your score: <span></span></p>
           <button class="btn-restart">Restart</button>
         </main>
+        <footer>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a></div>
+        </footer>
       `);
 
     var button = gameOverScreen.querySelector('button');
--- src/player.js
+++ src/player.js
@@ -4,11 +4,13 @@ function Player(canvas, lives) {
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
     this.lives = lives;
-    this.size = 60;
+    this.width = 80;
+    this.height = 60;
     this.x = canvas.width / 2;
-    this.y = canvas.height - this.size - 30;
+    this.y = canvas.height - this.height - 30;
     this.direction = 0;
     this.speed = 5;
+    this.image = new Image();
 
 }
 
@@ -32,26 +34,53 @@ Player.prototype.moveRight = function () {
 
 Player.prototype.didCollide = function (enemy) {
     var playerLeft = this.x;
-    var playerRight = this.x + this.size;
+    var playerRight = this.x + this.width;
     var playerTop = this.y;
-    var playerBottom = this.y + this.size;
+    var playerBottom = this.y + this.height;
 
     var enemyLeft = enemy.x;
-    var enemyRight = enemy.x + enemy.size;
+    var enemyRight = enemy.x + enemy.width;
     var enemyTop = enemy.y;
-    var enemyBottom = enemy.y + enemy.size * 2;
+    var enemyBottom = enemy.y + enemy.height * 2;
 
     // Check if the enemy intersects any of the player's sides
+    var crossLeft = enemyLeft <= playerRight && enemyLeft >= playerLeft;
     // var crossLeft = enemyLeft <= playerRight && enemyLeft >= playerLeft;
-    var crossLeft = enemyLeft <= playerRight && enemyRight >= playerRight;
 
-    // var crossRight = enemyRight >= playerLeft && enemyRight <= playerRight;
-    var crossRight = enemyRight >= playerRight && enemyLeft <= playerRight;
+    var crossRight = enemyRight >= playerLeft && enemyRight <= playerRight;
+    // var crossRight = enemyRight >= playerRight && enemyLeft <= playerRight;
 
-    var crossBottom = enemyBottom >= playerTop && enemyBottom < playerBottom;
+    var crossBottom = enemyBottom >= playerTop && enemyBottom <= playerBottom;
 
-    // var crossTop = enemyTop <= playerBottom && enemyTop >= playerTop;
-    var crossTop = enemyTop <= playerBottom && enemyBottom >= playerTop;
+    var crossTop = enemyTop <= playerBottom && enemyTop >= playerTop;
+    //var crossTop = enemyTop <= playerBottom && enemyBottom >= playerTop;
+
+    if ((crossLeft || crossRight) && (crossTop || crossBottom)) {
+        return true;
+    }
+    return false;
+};
+//KETCHUP
+Player.prototype.didCollide = function (ketchup) {
+    var playerLeft = this.x;
+    var playerRight = this.x + this.width;
+    var playerTop = this.y;
+    var playerBottom = this.y + this.height;
+
+    var ketchupLeft = ketchup.x;
+    var ketchupRight = ketchup.x + ketchup.width;
+    var ketchupTop = ketchup.y;
+    var ketchupBottom = ketchup.y + ketchup.width * 2;
+
+    var crossLeft = ketchupLeft <= playerRight && ketchupLeft >= playerLeft;
+    // var crossLeft = ketchupLeft <= playerRight && ketchupLeft >= playerLeft;
+
+    var crossRight = ketchupRight >= playerLeft && ketchupRight <= playerRight;
+    // var crossRight = ketchupRight >= playerRight && ketchupLeft <= playerRight;
+
+    var crossBottom = ketchupBottom >= playerTop && ketchupBottom <= playerBottom;
+
+    var crossTop = ketchupTop <= playerBottom && ketchupTop >= playerTop;
 
     if ((crossLeft || crossRight) && (crossTop || crossBottom)) {
         return true;
@@ -75,21 +104,19 @@ Player.prototype.handleScreenCollision = function () {
     var screenRight = this.canvas.width;
 
     // how to stop the edge of the player exactly with the edge of the screen?
-    if (this.x > (screenRight - this.size)) this.x = screenRight - this.size;
+    if (this.x > (screenRight - this.width)) this.x = screenRight - this.width;
     else if (this.x <= screenLeft) this.x = 0;
 };
 
 Player.prototype.removeLife = function () {
     this.lives -= 1;
 };
+// this is for ketchup
+// Player.prototype.addScore = function () {
+//     this.score += 2000;
+// }
 
 Player.prototype.draw = function () {
-    this.ctx.fillStyle = '#66D3FA';
-    // fillRect(x, y, width, height)
-    this.ctx.fillRect(
-        this.x,
-        this.y,
-        this.size,
-        this.size,
-    );
+    this.image.src = '../img/eyes-burger.png';
+    this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
 };
